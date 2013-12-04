86dc6d63c0011bd31818e91d2bca4c560808a3dc
huangyu <diwang90@gmail.com>
2013-12-05 00:09:23 +0800

    separate baidu engine out from picDownload and use self.maxThreads=min(counts,maxThreas)

--- picDownloader.py
+++ picDownloader.py
@@ -2,111 +2,13 @@
 #-*- coding: utf8 -*-
 import BeautifulSoup,os,urllib2,urllib
 import sys,re,threading,getopt
+from baidu import *
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-class baseSearchEngine(object):
-    """define some API for each searchEngine"""
-
-    def __init__(self,keyWord):
-        self.keyWord=keyWord
-
-    def doSearch(self):
-        '''return a result html fd'''
-        pass
-    def getImgUrl(self):
-        '''return a wanted image url'''
-        pass
-    def getConstructor(self):
-        '''return a constructor that can return a img url 
-        every time'''
-        pass
-
-class baiduEngine(baseSearchEngine):
-    '''baidu pic search engine'''
-
-    def __init__(self,keyWord):
-        baseSearchEngine.__init__(self,keyWord)
-        self.getSearchUrl() #gen self.url and self.baseReqData
-        self.host='wap.baidu.com'
-        self.baseReqData['word']=keyWord
-        self.matchImgSrc=re.compile('src=(\S+([jJ][Pp][gG]|[pP][nN][gG]))')
-        self.IMGDIVCLASS='mt ct b'
-        self.NEXTPAGEDIVCLASS='wm lh pr'
-
-    def doSearch(self):
-        '''return fd relate to search'''
-        url=self.url+'?'+urllib.urlencode(self.baseReqData)
-        print url
-        try:
-            self.resultFd=urllib2.urlopen(url)
-        except :
-            print("url open error")
-        return self.resultFd
-
-    def getImgUrl(self):
-        '''retun a img url every time called'''
-        self.doSearch()
-        html=self.resultFd.read()
-        while True:
-            onePageUrls,nextPageUrl=self.getOnePagePicUrl(html)
-            for i in xrange(len(onePageUrls)):
-                yield onePageUrls[i]
-            html=urllib2.urlopen(nextPageUrl).read()
-        
-
-    def getOnePagePicUrl(self,page):
-        '''get all needed pic urls on one page
-        the 'given' page is a string of webpage
-        return imgUrls list that contain imgurls
-        and nextPages url
-        '''
-        imgUrls=[]
-        nextPageUrl=None
-        soup=BeautifulSoup.BeautifulSoup(page)
-        wantedDiv=None
-        while (wantedDiv==None):
-                divs=soup.findAll('div')
-                for i in divs:
-                    self.__genAttrMap(i)
-                    iClass=i.attrMap.get('class',0)
-                    if(iClass==self.IMGDIVCLASS):
-                        wantedDiv=i
-                    elif(iClass==self.NEXTPAGEDIVCLASS):
-                        aUrls=i.findAll('a')
-                        for aUrl in aUrls:
-                            if(aUrl.getText()==u'下一页'):
-                                nextPageUrl='http://'+self.host+'/'+aUrl['href']
-                    else:
-                        del i
-        urls=wantedDiv.findAll('a')
-        for i in urls:
-            self.__genAttrMap(i)
-            imgUrls.append(self.matchImgSrc.search(i.attrMap['href']).groups()[0])
-        return imgUrls,nextPageUrl
-
-    def getSearchUrl(self):
-        '''generate self.url and self.baseReqData'''
-
-        hostUrl='http://wap.baidu.com/img'
-        try:
-            html=urllib2.urlopen(hostUrl).read()
-        except:
-            print('searchUrl open error')
-        soup=BeautifulSoup.BeautifulSoup(html)
-        form=soup.find('form')
-        self.__genAttrMap(form)
-        self.url='http://wap.baidu.com/'+form.attrMap['action']
-        inputSegs=form.findAll('input')
-        for i in inputSegs:
-            i.attrMap=dict(j for j in i.attrs)
-        self.baseReqData=dict((i.attrMap.get('name'),i.attrMap.get('value',0)) for i in inputSegs)
-
-    def __genAttrMap(self,soup):
-        soup.attrMap=dict(i for i in soup.attrs)
 
 class picDownloader(object):
-    def __init__(self,keyWord,counts,searchEngine=baiduEngine,path=".",maxThreads=10):
+    def __init__(self,keyWord,counts,path=".",searchEngine=baiduEngine,maxThreads=10):
         """
         if the parameter selfSearchEngine not Null,use it as
         searchEngine ,and the "counts" defines how much pic to
@@ -115,7 +17,7 @@ class picDownloader(object):
         self.keyWord=keyWord
         self.downloadedCounts=0
         self.path=path
-        self.maxThreads=maxThreads
+        self.maxThreads=min(maxThreads,counts)
         self.searchEngine=searchEngine
         self.counts=counts
 
@@ -137,7 +39,7 @@ class picDownloader(object):
             i+=1 
         while(downloadThread.preDownloadedCounts-downloadThread.downloadedCounts-1):
             imgurl=urlGenerator.next()
-            t=downloadThread(imgurl,self.path+'/'+str(i)+self.getImgType(imgurl))
+            t=downloadThread(imgurl,self.path+os.sep+str(i)+self.getImgType(imgurl))
             t.setDaemon(True)
             t.start()
             t.join()
@@ -197,8 +99,8 @@ if __name__=='__main__':
         print('You MUST specify the "-w keyword" and "-c counts" option')
         sys.exit(2)
     elif(optDict.get('-p',0)!=0): #has -p option
-        d=picDownloader(optDict['-w'],optDict['-c'],path=optDict['-p'])
+        d=picDownloader(optDict['-w'],int(optDict['-c']),path=optDict['-p'])
     else:
-        d=picDownloader(optDict['-w'],optDict['-c'])
+        d=picDownloader(optDict['-w'],int(optDict['-c']))
     d.startDownload()
 
